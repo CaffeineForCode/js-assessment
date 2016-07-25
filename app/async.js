1227e9e6e3bc9c155da2c03eef84b1db1ce5812b
CaffeineForCode <mrigank.mittal@nyu.edu>
2016-07-24 22:29:02 -0400

    update async and objects

--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+    var deferr = $.Deferred();
+    setTimeout(function () {
+      deferr.resolve(value);
+
+    },10);
+    return deferr.promise();
 
   },
 
   manipulateRemoteData: function(url) {
+    var deffer = $.Deferred();
+    $.ajax(url).then(function(resp){
+      var people = $.map(resp.people, function (person) {
+        return person.name;
 
+      });
+      deffer.resolve(people.sort());
+    });
+    return deffer.promise();
   }
 };
--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,21 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
-
+    alterContext: function(funct, obj) {
+        return funct.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+      var val = [];
+      for(var property in obj){
+        if(obj.hasOwnProperty(property)){
+        val.push(property + ': ' + obj[property]);
+        }
+      }
+    return val;
   }
 };
